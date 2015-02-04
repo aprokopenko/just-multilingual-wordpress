e0624bd8a28299519be0722d5c29e2fd68d34851
jc_asolomka <asolomka@justcoded.com>
2015-02-04 16:44:43 +0200

    done post type config page

--- views/network_language_posttypes.php
+++ views/network_language_posttypes.php
@@ -2,24 +2,21 @@
 <div class="wrap">
 	<h2>Language settings</h2>
 	<p>You can control language mapping for your sites here.</p>
-	
-	
-	<table>
-	<?php foreach($post_types as $name => $label): ?>
-	<!--
-		<tr>
-			<td><input type="checkbox" name="posttype[<?php echo $name; ?>]"><?php echo $label; ?></td>
-		</tr>
-	-->
-	<?php endforeach; ?>
-		<tr>
-			<td><input type="submit" value="Save" class="button button-primary"></td>
-		</tr>
-		
-	</table>
-	<?php 
-	pa($post_types);
-	
 
-	?>
+
+	<table>
+		<form action="?page=jcml-lang-posttypes#save" method="post" class="posttype-form">
+			<?php foreach( $post_types as $slug => $arr ): ?>
+
+				<tr>
+					<td><input type="checkbox" name="posttype[<?php echo $slug; ?>]" <?php echo $arr['checked'] == 1 ? 'checked' : ''; ?>><?php echo $arr['label']; ?></td>
+				</tr>
+
+			<?php endforeach; ?>
+			<tr>
+				<td><input type="submit" value="Save" class="button button-primary"></td>
+			</tr>
+
+	</table>
+
 </div>
